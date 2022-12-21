19167d5bcf59c3ed1f4cc6622d68bc95dbe90681
Siaff <frost.sunds@gmail.com>
2022-12-21 18:14:40 +0100

    Formatting

--- Uppgift 1/main.js
+++ Uppgift 1/main.js
@@ -1,51 +1,51 @@
 document.write(
-  "Redovisning av Uppgift 1_1 <br> Informatik-Systemutveckling, Malmö universitet, 2022-11-25 <br><br> Elias Frostensson, datorid: an3899 <br> ========================================================== <br><br>"
+    "Redovisning av Uppgift 1_1 <br> Informatik-Systemutveckling, Malmö universitet, 2022-11-25 <br><br> Elias Frostensson, datorid: an3899 <br> ========================================================== <br><br>"
 );
 
 function calc(tal1, op, op2, tal2) {
-  // Calc Operation
-  let svar = []; // Svar Array
-  console.log(tal1);
-  console.log(tal2);
-  console.log("parse:", parseInt(tal1 + op2 + tal2));
-  document.write(
-    `Vald operator: ${tal1} ${op2} ${tal2} = ${eval(
-      tal1 + op2 + tal2
-    )} <br><br>`
-  );
-  for (i = 0; i < op.length; i++) {
-    // for loop igenom alla operations.
-    svar.push(eval(tal1 + op[i] + tal2)); // Uträkning och push in i arrayn.
-    if (svar[i] == Infinity) {
-      // Kollar om man har delat något på 0.
-      document.write(`${tal1} ${op[i]} ${tal2} = Omöjligt<br>`);
-      return;
-    } else {
-      // Skriver ut resultaten
-      console.log("parse:", parseInt(tal1 + op[i] + tal2));
-      document.write(`${tal1} ${op[i]} ${tal2} = ${svar[i]} <br>`);
-      console.log("svar:", svar);
+    // Calc Operation
+    let svar = []; // Svar Array
+    console.log(tal1);
+    console.log(tal2);
+    console.log("parse:", parseInt(tal1 + op2 + tal2));
+    document.write(
+        `Vald operator: ${tal1} ${op2} ${tal2} = ${eval(
+            tal1 + op2 + tal2
+        )} <br><br>`
+    );
+    for (i = 0; i < op.length; i++) {
+        // for loop igenom alla operations.
+        svar.push(eval(tal1 + op[i] + tal2)); // Uträkning och push in i arrayn.
+        if (svar[i] == Infinity) {
+            // Kollar om man har delat något på 0.
+            document.write(`${tal1} ${op[i]} ${tal2} = Omöjligt<br>`);
+            return;
+        } else {
+            // Skriver ut resultaten
+            console.log("parse:", parseInt(tal1 + op[i] + tal2));
+            document.write(`${tal1} ${op[i]} ${tal2} = ${svar[i]} <br>`);
+            console.log("svar:", svar);
+        }
     }
-  }
-  miniResult(svar); // Kör miniResultat funktionen med svar arrayn.
-  return;
+    miniResult(svar); // Kör miniResultat funktionen med svar arrayn.
+    return;
 }
 
 function miniResult(answerArr) {
-  let miniRes = Infinity; // Går inte eftersom vi redan har eliminerat detta svaret i funktionen ovan.
-  for (i = 0; i < answerArr.length; i++) {
-    // Loopar genom hela svar.length
-    if (miniRes == Infinity) {
-      // Kollar om ett nummer har lagts in än annars lägg första nummret i array:n in.
-      miniRes = answerArr[i];
-    } else if (miniRes > answerArr[i]) {
-      // Om miniRes är större än det svaret man är på byt ut siffran.
-      miniRes = answerArr[i];
+    let miniRes = Infinity; // Går inte eftersom vi redan har eliminerat detta svaret i funktionen ovan.
+    for (i = 0; i < answerArr.length; i++) {
+        // Loopar genom hela svar.length
+        if (miniRes == Infinity) {
+            // Kollar om ett nummer har lagts in än annars lägg första nummret i array:n in.
+            miniRes = answerArr[i];
+        } else if (miniRes > answerArr[i]) {
+            // Om miniRes är större än det svaret man är på byt ut siffran.
+            miniRes = answerArr[i];
+        }
     }
-  }
-  document.write("- - - - - - - - - - - - - - - - <br>");
-  document.write("Minsta värdet är: ", miniRes); // Skriver ut minsta värdet.
-  return;
+    document.write("- - - - - - - - - - - - - - - - <br>");
+    document.write("Minsta värdet är: ", miniRes); // Skriver ut minsta värdet.
+    return;
 }
 
 let op = ["+", "-", "*", "/"]; // Operationsen
--- Uppgift 4/main.js
+++ Uppgift 4/main.js
@@ -1,5 +1,5 @@
 document.write(
-  "Redovisning av Uppgift 1_4 <br> Informatik-Systemutveckling, Malmö universitet, 2022-11-25 <br><br> Elias Frostensson, datorid: an3899 <br> ========================================================== <br><br>"
+    "Redovisning av Uppgift 1_4 <br> Informatik-Systemutveckling, Malmö universitet, 2022-11-25 <br><br> Elias Frostensson, datorid: an3899 <br> ========================================================== <br><br>"
 );
 
 // Kategorier av drycker med drycker i sig. Som const för de ändras inte och ska inte ändras.
@@ -10,47 +10,47 @@ const cold = ["Lemonade", "Juice", "Smoothie", "Milkshake", "Ice tea"];
 const drinks = [alcoholic, soft, hot, cold];
 
 function printDrinks(choice) {
-  // Skriv ut en kategori med drycker.
-  let getDrinks = drinks[choice]; // Hitta vilken katergori.
-  console.log(choice, " Drinks: ", getDrinks);
-  for (i = 0; i < getDrinks.length; i++) {
-    // Loopar igenom den valda kategorin av drycker och skriver ut alla.
-    document.write(getDrinks[i], "<br>");
-  }
+    // Skriv ut en kategori med drycker.
+    let getDrinks = drinks[choice]; // Hitta vilken katergori.
+    console.log(choice, " Drinks: ", getDrinks);
+    for (i = 0; i < getDrinks.length; i++) {
+        // Loopar igenom den valda kategorin av drycker och skriver ut alla.
+        document.write(getDrinks[i], "<br>");
+    }
 }
 
 function printAllDrinks() {
-  // Skriver ut alla drycker.
-  for (i = 0; i < drinks.length; i++) {
-    // Loopar igenom alla kategorier av drycker
-    let catDrink = drinks[i];
-    for (p = 0; p < catDrink.length; p++) {
-      // Loopar igenom alla drycker i en kategori.
-      document.write(catDrink[p], "<br>"); // Skriver ut
+    // Skriver ut alla drycker.
+    for (i = 0; i < drinks.length; i++) {
+        // Loopar igenom alla kategorier av drycker
+        let catDrink = drinks[i];
+        for (p = 0; p < catDrink.length; p++) {
+            // Loopar igenom alla drycker i en kategori.
+            document.write(catDrink[p], "<br>"); // Skriver ut
+        }
+        document.write("- - - - - - - - - - - - - - - - <br>");
     }
-    document.write("- - - - - - - - - - - - - - - - <br>");
-  }
 }
 
 function searchForDrink() {
-  // Söker efter dryck
-  let searchBar = window.prompt("Vad vill du söka på?");
-  let keyWord = searchBar.toLowerCase(); // Så man slipper ha stor bokstav i början.
-  for (i = 0; i < drinks.length; i++) {
-    // loopar igenom alla kategorier.
-    let catDrink = drinks[i];
-    for (p = 0; p < catDrink.length; p++) {
-      // Loopar igenom drycker i en kategori.
-      if (keyWord === catDrink[p].toLowerCase()) {
-        // Jämnför varje dryck i kategorin med vad användaren har skrivit in.
-        alert(`${catDrink[p]} finns i lagret!`);
-        return;
-      } else {
-        console.log("No Match");
-      }
+    // Söker efter dryck
+    let searchBar = window.prompt("Vad vill du söka på?");
+    let keyWord = searchBar.toLowerCase(); // Så man slipper ha stor bokstav i början.
+    for (i = 0; i < drinks.length; i++) {
+        // loopar igenom alla kategorier.
+        let catDrink = drinks[i];
+        for (p = 0; p < catDrink.length; p++) {
+            // Loopar igenom drycker i en kategori.
+            if (keyWord === catDrink[p].toLowerCase()) {
+                // Jämnför varje dryck i kategorin med vad användaren har skrivit in.
+                alert(`${catDrink[p]} finns i lagret!`);
+                return;
+            } else {
+                console.log("No Match");
+            }
+        }
     }
-  }
-  alert("Finns inte i lagret!"); // Sökordet fanns inte.
+    alert("Finns inte i lagret!"); // Sökordet fanns inte.
 }
 
 let fraga = window.prompt(`Which products are you looking for? 
@@ -64,17 +64,17 @@ let fraga = window.prompt(`Which products are you looking for?
 let val = parseInt(fraga); // 32 bit integer.
 
 if (val <= 3) {
-  // Om man väljer under eller 3 kör printDrinks().
-  console.log("Below or equal to 3");
-  printDrinks(val);
+    // Om man väljer under eller 3 kör printDrinks().
+    console.log("Below or equal to 3");
+    printDrinks(val);
 } else if (val === 4) {
-  // Om man väljer 4 kör printAllDrinks().
-  console.log("4 –––––");
-  printAllDrinks();
+    // Om man väljer 4 kör printAllDrinks().
+    console.log("4 –––––");
+    printAllDrinks();
 } else if (val === 5) {
-  // Om man väljer 5 kör searchForDrink().
-  console.log("5 –––––");
-  searchForDrink();
+    // Om man väljer 5 kör searchForDrink().
+    console.log("5 –––––");
+    searchForDrink();
 } else {
-  alert("You did something wrong!");
+    alert("You did something wrong!");
 }
